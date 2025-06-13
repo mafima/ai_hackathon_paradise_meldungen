b685d7229933ab466b9abc94ba65870823bee5ca
manu <mutinteractive@gmail.com>
2025-06-13 21:38:19 +0200

    add counter

--- count_links.py
+++ count_links.py
@@ -1,18 +1,38 @@
 import socket
 import time
+import os
+
+
+# put all data unzipped in the data folder
+
 
 link = "https://shorturl.at/hcHtb"
 path = "/home/manu/DEV/AI/ai_hackathon_paradise_meldungen/data"
+path_add = "/maengel/maengel"
+path_final = path + path_add # This is the correct way to append
 
-#
-# please put all zips to data folder.
 
+# JSON Format Example:
+data_json = {
+    "title": "#123",
+    "service_request_id": "3",
+    "description": "<p> text </p>\n",
+    "media_url": "",
+    "service_name": "Straße, ",
+    "service_code": "12_straße",
+    "processing_status": "Geschlossen",
+    "geolocation_latitude": "12.3",
+    "geolocation_longitude": "11.1",
+    "requested_datetime": "<time datetime=\"2018</time>\n",
+    "updated_datetime": "<time datetime=\"2021</time>\n"
+}
 
 def run_client():
-
+    print("Client start.")
     # count all json containing html tag.
     filter_string = "<hlink="
-
+    count = sum(1 for file in os.listdir(path_final) if filter_string in file)
+    print(f"Number of JSON files with '{'<hlink=':', count}")
     print("Client closed.")
 
 if __name__ == "__main__":
